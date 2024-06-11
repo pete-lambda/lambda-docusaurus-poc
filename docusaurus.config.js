459476592dfa3c97e97f1ae9cb11906fe2c08609
Pete B <pete.bratach@lambdal.com>
2024-06-11 09:47:55 -0700

    Fix url
    Need only the base URL here

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://pete-lambda.github.io/lambda-docusaurus-poc',
+  url: 'https://pete-lambda.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
